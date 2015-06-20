e1beec586b435fca432e56ff1519839ec4bc0592
Serge Seletskyy <sergey@clcoudconsulting.no>
2015-06-20 20:05:15 +0300

    set rescue_from :all to catch exceptions

--- app/api/demo/root.rb
+++ app/api/demo/root.rb
@@ -3,6 +3,24 @@ module Demo
   class Root < Grape::API
     prefix 'api'
 
+    rescue_from :all do |e|
+      # Rollbar.warning("API returned code 418", e) if Rails.env.production?
+      if Rails.env.test?
+        puts '=============== EXCEPTION =================='
+        puts e.inspect
+        puts '=============== BACKTRACE =================='
+        # grape-api is the part of the current name of the project
+        #                                       |
+        puts e.backtrace.select { |x| x.match(/grape-api/) }
+        puts '===============    END    =================='
+      end
+
+      Rack::Response.new({status_code: 418,
+                          error_code: "4018",
+                          detail: e.message
+                         }.to_json, 418)
+    end
+
     helpers do
       include GrapeHelper
 
